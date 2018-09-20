921c96f02c7109c27b67ce798f0a599e749df8ef
997850262 <997850262@qq.com>
2018-09-20 18:16:11 +0800

    .

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import './App.css';
 import configureStore from './store/configureStore';
 /* eslint-disable */
 import { Router, browserHistory  } from 'react-router';
-import routes from './routes';
+// import routes from './routes';
+import Music from './Container/Music';
 
 const store = configureStore();
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router routes={routes} history={browserHistory } />
+        {/* <Router routes={routes} history={browserHistory } /> */}
+        <Music />
       </Provider>
     );
   }
